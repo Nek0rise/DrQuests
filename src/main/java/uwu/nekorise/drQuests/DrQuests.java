a98d0eb2252c8fdcdb2071acfcdd72efcb271b2b
Nekorise <you@example.com>
2026-02-12 21:30:04 +0500

    init

---
+++ src/main/java/uwu/nekorise/drQuests/DrQuests.java
@@ -0,0 +1,13 @@
+package uwu.nekorise.drQuests;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DrQuests extends JavaPlugin {
+    @Getter private static DrQuests instance;
+
+    @Override
+    public void onEnable() {
+
+    }
+}
